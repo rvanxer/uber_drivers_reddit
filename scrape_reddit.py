d3f79b61b52dcfa96552874b6a00008e17cda499
RajatVerma <emphasent@gmail.com>
2020-05-25 08:50:31 -0400

    Collected data of top posts plus keyword search
    Assembled the union of top 1000 posts by different measures - topmost, hottest, newest - and results of explicit search of a few keywords related to striking/organizing. It is not difficult to extend the obtained dataset by extending this list of search keywords.

--- scrape_reddit.py
+++ scrape_reddit.py
@@ -1,20 +1,39 @@
 #! python3
 """Created: Tue May 19 16:06:52 2020 @author: Rajat Verma"""
-
+#%%
 import praw
 import csv
 from datetime import datetime as time
 
 # create the praw Reddit object
-red = praw.Reddit(client_id = 'wejqi7u3P_dE4A',
-                  client_secret = '21iuFXgESnIg4BaUMzSSt6yinAA',
-                  user_agent = 'uber_drivers_reddit',
-                  username = 'emphasent',
-                  password = 'Wje3Rj!ShVZ9dPg')
+red = praw.Reddit(client_id='wejqi7u3P_dE4A',
+                  client_secret='21iuFXgESnIg4BaUMzSSt6yinAA',
+                  user_agent='uber_drivers_reddit',
+                  username='emphasent',
+                  password='Wje3Rj!ShVZ9dPg')
 # get the Uber drivers' subreddit
 sub = red.subreddit('uberdrivers')
 
+#%% Setup for reading the data
+# define the relevant fields of the objects & files to save them
+fields = {
+    'thd': ['id','created','num_comments',
+          'permalink','score','title','upvote_ratio'],
+    'msg': ['id','link_id','created_utc','is_submitter',
+            'parent_id','score','body'],
+    'auth': ['id','name','comment_karma','created_utc',
+             'fullname','is_mod','is_gold','link_karma']
+}
+files = {
+    'thd': 'data/threads.csv',
+    'msg': 'data/messages.csv',
+    'auth': 'data/authors.csv'
+}
+keywords = ['strike','protest']
+
+# helper functions
 def log(msg, file='data_read_errors.log'):
+    '''Log the read errors (not using `logging` coz I suck at it)'''
     with open(file, 'a') as f:
         f.write('%s: %s\n' % (time.now().isoformat(), msg))
 
@@ -28,70 +47,9 @@ def get_obj_info(obj, fields):
             res[field] = None
     return res
 
-# define the fields the details
-fields = {
-    'thd': ['id','created','num_comments',
-          'permalink','score','title','upvote_ratio'],
-    'msg': ['id','link_id','created_utc','is_submitter',
-            'parent_id','score','body'],
-    'auth': ['id','name','comment_karma','created_utc',
-             'fullname','is_mod','is_gold','link_karma']
-}
-
-files = {
-    'thd': 'data/threads.csv',
-    'msg': 'data/messages.csv',
-    'auth': 'data/authors.csv'
-}
-
-#def read_submissions(top, save=False):
-#    thds = []
-#    msgs = []
-#    auths = {}
-#    for post in sub.top(limit = top):
-#        # get the thread details
-#        thd = get_obj_info(post, fields['thd'])
-#        if post.author is not None:
-#            thd['auth_id'] = post.author.id
-#            auths[post.author.id] = get_obj_info(post.author, fields['auth'])
-#        else:
-#            thd['auth_id'] = ''
-#        thds.append(thd)
-#        # get the author details
-#        
-#        # get the messages/comments
-#        for com in post.comments:
-#            try:
-#                msg = get_obj_info(com, fields['msg'])
-#                # get the author details
-#                if com.author is not None:
-#                    msg['auth_id'] = com.author.id
-#                    auth = get_obj_info(com.author, fields['auth'])
-#                    auths[com.author.id] = auth
-#                else:
-#                    msg['auth_id'] = ''
-#                msgs.append(msg)
-#            except TypeError as e:
-#                # logging.debug('msg[{}]:{}'.format(com.id, e))
-#                log('msg[{}]:{}'.format(com.id, e))
-#                # pass
-#            except AttributeError as e:
-#                # logging.debug('msg[{}]:{}'.format(com.id, e))
-#                log('msg[{}]:{}'.format(com.id, e))
-#                # pass
-#    
-#    thds = pd.DataFrame(thds).set_index('id')
-#    msgs = pd.DataFrame(msgs).set_index('id')
-#    auths = pd.DataFrame(list(auths.values())).set_index('id')
-#    
-#    if save:
-#        thds.to_csv(files['thd'], index=False)
-#        msgs.to_csv(files['msg'], index=False)
-#        auths.to_csv(files['auth'], index=False)
-#    
-#    return thds, msgs, auths
-
 def read_write_submissions(subreddit, top):
+    '''Read top submissions and save them to disk 
+    (without storing in memory)'''
     # open the files
     f_thd = open(files['thd'], 'w', newline='', encoding='utf-8')
     f_msg = open(files['msg'], 'w', newline='', encoding='utf-8')
@@ -109,9 +67,7 @@ def read_write_submissions(subreddit, top):
     post_num = 0
     for post in subreddit.top(limit=top):
         post_num += 1
-        if post_num < 1000:
-            continue
-        print('Post#%d' % post_num)
+        print('#%d:' % post_num, post)
         try:
             # get the thread details
             thd = get_obj_info(post, fields['thd'])
@@ -147,8 +103,20 @@ def read_write_submissions(subreddit, top):
     f_msg.close()
     f_auth.close()
 
+def get_posts_by_keyword(subreddit, keywords, limit=1000,
+                        methods=['relevance','top','hot','new']):
+    '''Get submission objects based on search query of keywords'''
+    data = {}
+    for term in keywords:
+        data[term] = {}
+        for method in methods:
+            generator = subreddit.search(term, sort=method, limit=limit)
+            data[term][method] = set(generator)
+    return data
+
 #%% Main
 t = time.now()
-read_write_submissions(sub, top=10000)
-print('Time elapsed: %.3fs' % (time.now() - t).total_seconds())
+# read_write_submissions(sub, top=10000)
+kwData = get_posts_by_keyword(sub, ['strike','protest'])
+print('Time elapsed: %.3f s' % (time.now() - t).total_seconds())
 # time taken for top 1000 submissions: 20,604.5 s = 5.72 h
